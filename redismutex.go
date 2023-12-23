a325c07d5e0a8199aeeb840453bd5e1c6405ebcf
Hao Jia <34637423+lihaojia24@users.noreply.github.com>
2023-12-23 10:46:40 +0800

    update redismutex

--- redismutex.go
+++ redismutex.go
@@ -11,15 +11,16 @@ type RedisMutex struct {
 	rdb      *redis.Client
 	identity string
 	lockname string
+	ticker   *time.Ticker
 }
 
 func NewRedisMutex(identity, lockname string) *RedisMutex {
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
-		Password: "abc123", // no password set
-		DB:       0,        // use default DB
+		Password: "abc123",
+		DB:       0,
 	})
-	return &RedisMutex{rdb, identity, lockname}
+	return &RedisMutex{rdb, identity, lockname, nil}
 }
 
 func (m *RedisMutex) TryLock() bool {
@@ -28,7 +29,40 @@ func (m *RedisMutex) TryLock() bool {
 	if err != nil {
 		return false
 	}
+	m.ticker = time.NewTicker(time.Duration(time.Second * 2))
+	go func() {
+		for {
+			_, ok := <-m.ticker.C
+			if ok {
+				m.rdb.SetEx(ctx, m.lockname, m.identity, time.Duration(3*time.Second))
+			} else {
+				break
+			}
+		}
+	}()
 	return ok
+}
+
+func (m *RedisMutex) Lock() {
+	ctx := context.Background()
+	for {
+		ok, err := m.rdb.SetNX(ctx, m.lockname, m.identity, time.Duration(3*time.Second)).Result()
+		if err == nil && ok {
+			break
+		}
+		time.Sleep(time.Second)
+	}
+	m.ticker = time.NewTicker(time.Duration(time.Second * 2))
+	go func() {
+		for {
+			_, ok := <-m.ticker.C
+			if ok {
+				m.rdb.SetEx(ctx, m.lockname, m.identity, time.Duration(3*time.Second))
+			} else {
+				break
+			}
+		}
+	}()
 }
 
 func (m *RedisMutex) Unlock() bool {
@@ -36,6 +70,9 @@ func (m *RedisMutex) Unlock() bool {
 	identity, err := m.rdb.Get(ctx, m.lockname).Result()
 	if err != nil {
 		return false
+	}
+	if m.ticker != nil {
+		m.ticker.Stop()
 	}
 	if identity != m.identity {
 		return false
--- tests/redismutex_test.go
+++ tests/redismutex_test.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/lihaojia24/go-xtools"
 )
@@ -10,9 +11,13 @@ import (
 func Test_redisMutex(t *testing.T) {
 	m1 := xtools.NewRedisMutex("m1", "lockname")
 	m2 := xtools.NewRedisMutex("m2", "lockname")
-	fmt.Printf("m1.TryLock(): %v\n", m1.TryLock())
+	m1.Lock()
+	m1.Unlock()
+	// fmt.Printf("m1.TryLock(): %v\n", m1.TryLock())
 	// time.Sleep(time.Second * 5)
-	fmt.Printf("m2.TryLock(): %v\n", m2.TryLock())
+	m2.Lock()
+	// fmt.Printf("m2.TryLock(): %v\n", m2.TryLock())
 	fmt.Printf("m1.Unlock(): %v\n", m1.Unlock())
 	fmt.Printf("m2.Unlock(): %v\n", m2.Unlock())
+	time.Sleep(time.Second)
 }
